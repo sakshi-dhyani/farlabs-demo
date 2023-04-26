e2e88e32fc226e74f53efd0e81c10afd86a9a49c
sakshi-dhyani <ibs.sakshi9@gmail.com>
2023-04-26 11:23:46 +0530

    update test case

--- src/main/java/com/farelabs/controller/SampleController.java
+++ src/main/java/com/farelabs/controller/SampleController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -27,6 +28,11 @@ import com.farelabs.service.SampleService;
 public class SampleController {
 	
 	@Autowired private SampleService sampleService;
+
+	@RequestMapping("/hello")
+	public String hello(){
+		return "testing success";
+	}
 	
 	@PostMapping("/bd/addSampleDetails")
 	public ResponseEntity<Response>addSampleDetails(@RequestParam (value ="name" , required=true )String name,
